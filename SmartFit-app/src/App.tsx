822065880163faa8fed29a23c1f1b89014d68fc0
Lucia Supova <luciasupovaa@gmail.com>
2024-11-10 15:45:38 +0100

    Navbar

--- SmartFit-app/src/App.tsx
+++ SmartFit-app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
+import Navbar from './components/Navbar/Navbar';
+import logoImg from './assets/Workout-icon.svg'; 
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Navbar logoImg={logoImg} logoAlt={'Logo'} />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
--- SmartFit-app/src/components/Navbar/Navbar.tsx
+++ SmartFit-app/src/components/Navbar/Navbar.tsx
@@ -1,1 +1,43 @@
-import './Navbar.module.scss'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.scss';
+import loginIcon from '../../assets/Log-icon.svg';
+
+type NavbarProps = {
+  logoImg: string;
+  logoAlt: string;
+};
+
+function Navbar({ logoImg, logoAlt }: NavbarProps) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={logoImg} alt={logoAlt} className={styles.logoImage} />
+        </Link>
+      </div>
+
+      {/*Burger menu lines*/}
+      <div className={styles.burger} onClick={toggleMenu}>
+        <span className={`${styles.line} ${menuOpen ? styles.lineOpen : ''}`}></span>
+        <span className={`${styles.line} ${menuOpen ? styles.lineOpen : ''}`}></span>
+        <span className={`${styles.line} ${menuOpen ? styles.lineOpen : ''}`}></span>
+      </div>
+
+      {/*Menu links*/}
+      <nav className={`${styles.navLinks} ${menuOpen ? styles.menuOpen : ''}`}>
+        <Link to="/login">
+          <img src={loginIcon} alt="Login Icon" className={styles.icon} /> Login
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
