b88966dfafe298f382d56512d57d6fea14c3413a
Lucia Supova <luciasupovaa@gmail.com>
2024-11-18 14:04:41 +0100

    .

--- SmartFit-app/src/pages/Recording/Recording.tsx
+++ SmartFit-app/src/pages/Recording/Recording.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import '../../pages/Recording/Recording.scss';
-import Button from '../../components/Button/Button';
-import NoIcon from '../../assets/No-icon.svg';
-import Header from '../../components/Header/Header';
-import ErrorPrevention from '../../components/ErrorPrevention/ErrorPrevention';
-import PoseDetection from '../../components/PoseDetection/PoseDetection';
+import React, { useState } from "react";
+import "../../pages/Recording/Recording.scss";
+import Button from "../../components/Button/Button";
+import NoIcon from "../../assets/No-icon.svg";
+import Header from "../../components/Header/Header";
+import ErrorPrevention from "../../components/ErrorPrevention/ErrorPrevention";
+import PoseDetection from "../../components/PoseDetection/PoseDetection";
 
-const Recording = () => {  
+const Recording = () => {
   const [showErrorPrevention, setShowErrorPrevention] = useState(false);
 
   const handleEndClick = () => {
-    setShowErrorPrevention(true); 
+    setShowErrorPrevention(true);
   };
 
   const handleCloseModal = () => {
@@ -18,19 +18,12 @@ const Recording = () => {
   };
 
   return (
-    <div className='recordingPage'>
-      <Header
-        variant={'phoneM'}
-        title='Recording'
-      />
+    <div className="recordingPage">
+      <Header variant={"phoneM"} title="Recording" />
 
-      {/* Placeholder */}
-      <div className='recordingArea'>
-        <img 
-          src="https://via.placeholder.com/600x300?text=Recording+in+Progress" 
-          alt="Recording in progress" 
-          className='placeholderImageArea' 
-        />
+      {/* Replace the placeholder with PoseDetection */}
+      <div className="recordingArea">
+        <PoseDetection />
       </div>
 
       <Button
@@ -40,12 +33,11 @@ const Recording = () => {
         variant="red"
       />
 
-      <PoseDetection/>
       {showErrorPrevention && (
         <ErrorPrevention
           text="Are you sure you want to end the recording?"
           type="error"
-          onClose={handleCloseModal} 
+          onClose={handleCloseModal}
         />
       )}
     </div>
